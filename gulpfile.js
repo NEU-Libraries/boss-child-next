4d384103ae085cfdecf8335ab46063752f37fcf7
Ryan Williams <ryrowi@gmail.com>
2016-10-12 16:55:42 -0400

    remove redundant gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed', includePaths: ['./sass/**/*.scss']}).on('error', sass.logError))
+    .pipe(sass({includePaths: ['./sass/**/*.scss']}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
