e85c284340fef903a02caa1579d04b68b5592f52
Ryan Williams <ryrowi@gmail.com>
2016-11-17 10:42:40 -0500

    normalize whitespace in template-parts/header-middle-column.php

--- template-parts/header-middle-column.php
+++ template-parts/header-middle-column.php
@@ -47,16 +47,16 @@ $boxed = boss_get_option( 'boss_layout_style' );
     <?php endif; ?>
 
     <?php if( '2' == $header_style ): ?>
-    
+
     <div id="titlebar-search">
     <?php
-    get_template_part( 'searchform', 'header' ); 
+    get_template_part( 'searchform', 'header' );
     ?>
         <a href="#" id="search-open" class="header-button" title="<?php _e( 'Search', 'boss' ); ?>"><i class="fa fa-search"></i></a>
-    </div><!-- #titlebar-search-->   
-     
+    </div><!-- #titlebar-search-->
+
     <?php else: ?>
-    
+
 	<?php if ( $boxed == 'boxed' ) { ?>
 		<!-- search form -->
 		<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
@@ -64,9 +64,9 @@ $boxed = boss_get_option( 'boss_layout_style' );
 			<button type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
 		</form>
     <?php } ?>
-        
+
     <?php endif; ?>
-	
+
 <?php if( '1' == $header_style ) { ?>
 </div><!--.middle-col-->
 <?php } ?>
