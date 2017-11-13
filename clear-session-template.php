ab14f5e0fbbc3bf45fe94c65b310e15c7553e98b
Eric Knappe <eknappe@mla.org>
2017-11-13 16:08:30 +0000

    Reset redirect_to cookie on when clearing session.

--- clear-session-template.php
+++ clear-session-template.php
@@ -10,6 +10,7 @@
 	//must set cookies before header
 	setcookie( '_saml_idp', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), false, true );
 	setcookie( 'stickyIdPSelection', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
+	setcookie( 'redirect_to', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
 	wp_destroy_current_session();
 	wp_clear_auth_cookie();
 
--- page-clear-session.php
+++ page-clear-session.php
@@ -13,6 +13,7 @@ $shib_urls = [
 
 setcookie( '_saml_idp', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), false, true );
 setcookie( 'stickyIdPSelection', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
+setcookie( 'redirect_to', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
 wp_destroy_current_session();
 wp_clear_auth_cookie();
 
