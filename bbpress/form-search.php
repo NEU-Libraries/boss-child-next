ad30caa23c499a41e5e2f2a15ccc127b9503dc32
Nelson Alonso <nalonsopress@gmail.com>
2018-01-09 17:31:28 -0500

    Finalizing new forum search

--- bbpress/form-search.php
+++ bbpress/form-search.php
@@ -15,16 +15,17 @@ $group_slug = bp_get_current_group_slug();
 	<div>
 		<label class="screen-reader-text hidden" for="bbp_search"><?php _e( 'Search for:', 'bbpress' ); ?></label>
 		<!--<input type="hidden" name="action" value="bbp-search-request" />-->
-		<input tabindex="<?php bbp_tab_index(); ?>" type="text" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" id="bbp_search" />
-		
-                <?php if( $forum_id ): ?>
+
+		<?php if( $forum_id ): ?>
                     <input type="hidden" name="bbp_search_forum_id" value="<?php echo $forum_id; ?>" />
                 <?php endif; ?>
                  
                 <?php if( $group_slug ): ?>
                     <input type="hidden" name="bbp_search_group_slug" value="<?php echo $group_slug; ?>" />
                 <?php endif; ?> 
-     
+
+
+		<input tabindex="<?php bbp_tab_index(); ?>" type="text" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" id="bbp_search" />
                 <input tabindex="<?php bbp_tab_index(); ?>" class="button" type="submit" id="bbp_search_submit" value="<?php esc_attr_e( 'Search', 'bbpress' ); ?>" />
 
 	
