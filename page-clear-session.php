8590cd6ece9b3c8ddabca81c7348ed2637f5545f
Ryan Williams <rwilliams@mla.org>
2018-01-12 11:09:50 -0500

    make /clear-session destroy all WP session for the current user

--- page-clear-session.php
+++ page-clear-session.php
@@ -14,9 +14,11 @@ $shib_urls = [
 setcookie( '_saml_idp', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), false, true );
 setcookie( 'stickyIdPSelection', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
 setcookie( 'redirect_to', false, time()-3600, '/', '.' . getenv('WP_DOMAIN'), true, true );
-wp_destroy_current_session();
+
 wp_clear_auth_cookie();
 
+$manager = WP_Session_Tokens::get_instance( get_current_user_id() );
+$manager->destroy_all();
 
 get_header(); ?>
 
