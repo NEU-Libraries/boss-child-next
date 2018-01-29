48d6771c7728b3a91b402257224ff73918af2405
Ryan Williams <rwilliams@mla.org>
2018-01-29 12:31:46 -0500

    prevent php notice by only processing origin site link if the site exists

--- template-parts/header-middle-column.php
+++ template-parts/header-middle-column.php
@@ -9,14 +9,17 @@ if ( class_exists( 'Humanities_Commons' ) && ! empty( (new Humanities_Commons)->
 
 	if  ( ! empty( $shib_login_host ) ) {
 		$origin_site = get_site_by_path( $shib_login_host, '/' );
-		$origin_site_details = get_blog_details( $origin_site->blog_id );
 
-		if ( get_current_blog_id() !== (int) $origin_site->blog_id ) {
-			$back_to_network_link = sprintf(
-				'<a href="%s">Return to %s</a>',
-				$origin_site_details->siteurl,
-				$origin_site_details->blogname
-			);
+		if ( $origin_site ) {
+			$origin_site_details = get_blog_details( $origin_site->blog_id );
+
+			if ( get_current_blog_id() !== (int) $origin_site->blog_id ) {
+				$back_to_network_link = sprintf(
+					'<a href="%s">Return to %s</a>',
+					$origin_site_details->siteurl,
+					$origin_site_details->blogname
+				);
+			}
 		}
 	}
 }
