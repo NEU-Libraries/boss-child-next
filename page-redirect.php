e71eb6b5ac0b767f5d93f06cbedd19a09827d39a
Nelson Alonso <nalonsopress@gmail.com>
2018-03-26 16:09:23 -0400

    after phpcs

--- page-redirect.php
+++ page-redirect.php
@@ -1,31 +1,40 @@
-<?php /* Template Name: Page Redirect */
+<?php
+/**
+ * Template Name: Page Redirect
+ *
+ * @package Boss_Child
+ */
 
-if (function_exists('have_posts') && have_posts())
-{
-	// Prevent links turning into oembed
+if ( function_exists( 'have_posts' ) && have_posts() ) {
+	// Prevent links turning into oembed.
 	remove_filter( 'the_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
 
-	while (have_posts())
-	{
+	while ( have_posts() ) {
 		the_post();
 
 		ob_start();
 		the_content();
-		$contents	= ob_get_contents();
+		$contents = ob_get_contents();
 		ob_end_clean();
 
-		// grab the 'naked' link
-		$link = trim(strip_tags($contents));
+		// Grab the 'raw' link.
+		$link = trim( strip_tags( $contents ) );
 
-		if( ! preg_match('%^(http|https)://%', $link) ) {
-			$host	= $_SERVER['HTTP_HOST'];
-			$dir	= dirname( $_SERVER['PHP_SELF'] );
-			$link	= "https://$host$dir/$link";
+		if ( ! preg_match( '%^(http|https)://%', $link ) ) {
+
+			if ( isset( $_SERVER['HTTP_HOST'] ) ) {
+				$host = sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) );
+			}
+
+			if ( isset( $_SERVER['PHP_SELF'] ) ) {
+				$dir = dirname( sanitize_text_field( wp_unslash( $_SERVER['PHP_SELF'] ) ) );
+			}
+
+			$link = sprintf( "https://%1$s%2$s/%3$s", $host, $dir, $link );
 		}
 
-		header("HTTP/1.1 301 Moved Permanently");
-		header("Location: $link");
+		header( 'HTTP/1.1 301 Moved Permanently' );
+		header( "Location: $link" );
 		exit;
 	}
 }
-?>
