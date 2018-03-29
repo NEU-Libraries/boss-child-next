3b5b22b401b0590f6b320b17dc8adb2c45359c08
Nelson Alonso <nalonsopress@gmail.com>
2018-03-29 10:12:41 -0400

    Removing unnecessary link formatting

--- page-redirect.php
+++ page-redirect.php
@@ -20,19 +20,6 @@ if ( function_exists( 'have_posts' ) && have_posts() ) {
 		// Grab the 'raw' link.
 		$link = trim( strip_tags( $contents ) );
 
-		if ( ! preg_match( '%^(http|https)://%', $link ) ) {
-
-			if ( isset( $_SERVER['HTTP_HOST'] ) ) {
-				$host = sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) );
-			}
-
-			if ( isset( $_SERVER['PHP_SELF'] ) ) {
-				$dir = dirname( sanitize_text_field( wp_unslash( $_SERVER['PHP_SELF'] ) ) );
-			}
-
-			$link = sprintf( "https://%1$s%2$s/%3$s", $host, $dir, $link );
-		}
-
 		header( 'HTTP/1.1 301 Moved Permanently' );
 		header( "Location: $link" );
 		exit;
